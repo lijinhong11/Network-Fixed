7a4fc19bb1dc99df3c2e3e37dd848d80622f06ba
lijinhong11 <tygfhk@outlook.com>
2024-08-26 14:34:46 +0800

    fix ctrl+v with dripleaf dupe bug

--- src/main/java/io/github/sefiraat/networks/listeners/BlockListener.java
+++ src/main/java/io/github/sefiraat/networks/listeners/BlockListener.java
@@ -5,11 +5,16 @@ import io.github.sefiraat.networks.network.NetworkNode;
 import io.github.sefiraat.networks.network.NodeDefinition;
 import io.github.sefiraat.networks.network.NodeType;
 import io.github.sefiraat.networks.slimefun.network.NetworkController;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Bisected;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockListener implements Listener {
@@ -21,6 +26,19 @@ public class BlockListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent e) {
         removeNetwork(e.getBlock().getLocation());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockGrow(BlockGrowEvent e) {
+        Block block = e.getBlock();
+        if (block.getBlockData() instanceof Bisected) {
+            Location b1 = block.getLocation().add(0, 1, 0);
+            SlimefunItem item = BlockStorage.check(b1);
+            if (item != null) {
+                removeNetwork(b1);
+                BlockStorage.clearBlockInfo(b1);
+            }
+        }
     }
 
     private void removeNetwork(Location location) {
